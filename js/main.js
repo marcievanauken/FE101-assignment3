3dbae4252730fb8d33d7d58702deb718ab4d738e
Marcie Van Auken <marcievanauken@gmail.com>
2015-04-01 16:19:02 -0400

    javascript exercises

---
+++ js/main.js
@@ -0,0 +1,157 @@
+
+
+/* alert pop up */
+// alert ("Hello World!");
+
+
+
+
+/* Variable */
+// var x = "8";
+
+// if (x>=7)
+// {
+// console.log("bigger than 10")
+// }
+// else{
+// console.log("less than 10")
+// }
+
+
+
+
+/* Variable */
+// var taco = 4;
+// var burrito = 6;
+// console.log(taco+burrito)
+
+
+
+/* Variable */
+// var taco = "cat";
+// var burrito = "6";
+// console.log(taco+burrito)
+
+
+
+/* Variable */
+// var greeting = "Hello ";
+// var name = "Marcie";
+// alert(greeting + name);
+
+
+
+
+/* Alert */
+// alert(4+6)
+
+
+
+/* Array */
+// var myArray = [["cat","tiger"],["dog","wolf"]]
+// console.log(myArray[0]);
+// console.log(myArray[1][0]);
+// console.log(myArray[1][1]);
+
+
+/* Array */
+var colors = [
+	["pink", "blue", "green"],
+	["orange", "purple"]
+];
+
+console.log(colors[0][0]);
+console.log(colors[1][1]);
+
+
+
+
+/* Debugging */
+// var x = "red";
+
+// if (x === "red")
+// {
+// console.log("The color was red")
+// }
+// else{
+// console.log("nope")
+// }
+
+
+
+/* Logic */
+// if(5>10){
+// 	console.log("5 is not greater than 10");
+// }
+// if(5<10){
+// 	console.log("yes 5 is less than 10")
+// }
+
+
+
+/* Loop */
+// for(var i = 0; i<10; i++){
+// console.log(i)
+// }
+
+
+
+ // Loop 
+// beers = ["Lagunitas", "Peak"]
+// for(var i = 0; i< beers.length; i++){
+// console.log(beers[i])
+// }
+
+
+
+
+
+
+/* function */
+function change() {
+	document.getElementById('foo').innerHTML = 'NEW TEXT';
+}
+
+
+
+
+
+/* 3 functions that take input and return via alert or console */
+
+// function doorPrize(doorNumber){
+// 	if(doorNumber == 1){
+// 		console.log("you won big");
+
+// 	}else if(doorNumber == 2){
+// 		console.log("you won nothing");
+
+// 	}else{
+// 		console.log("stop that");
+// 	}
+// }
+
+// var door = window.prompt("choose a door")
+
+// doorPrize(door);
+
+
+
+// function cats(howManyCats){
+// 	if(howManyCats == 3){
+// 		console.log("sufficient amount of cats");
+	
+// 	}else if (howManyCats == 4){
+// 		console.log("getting to be too many cats");
+	
+// 	}else{
+// 		console.log("not enough or way too many cats");
+// 	}
+// }
+// var cat = window.prompt("how many cats are there")
+
+// cats(cat);
+
+
+
+
+
